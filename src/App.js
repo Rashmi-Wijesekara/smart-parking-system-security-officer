6e131ac480771c0e9cb0efd96eb4d7dc76148bbd
Rashmi <rashmiwijesekara1999@gmail.com>
2022-07-05 11:34:04 +0530

    react-transition & react-router-dom installed, router checked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import React from "react";
+import {
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+} from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="font-bold text-red-500 text-xl">hello world</div>
-    </div>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path="/" exact>
+					login page
+				</Route>
+
+				<Route path="/dashboard">dashboard page</Route>
+        <Route path="/error">404 not found</Route>
+				<Redirect to="/error" />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
