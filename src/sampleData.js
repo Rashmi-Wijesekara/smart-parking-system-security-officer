09d7231f161bd1b12d15a63441ee421b869458fd
Rashmi <rashmiwijesekara1999@gmail.com>
2022-07-23 17:42:57 +0530

    profile & parking log completed

---
+++ src/sampleData.js
@@ -0,0 +1,127 @@
+// shift log
+export const shiftLogData = [
+	{
+		id: "1",
+		date: "10-06-2022",
+		shiftType: "Day",
+		startTime: "08.28 am",
+		endTime: "05.28 pm",
+	},
+	{
+		id: "2",
+		date: "11-06-2022",
+		shiftType: "Day",
+		startTime: "08.31 am",
+		endTime: "05.34 pm",
+	},
+	{
+		id: "3",
+		date: "12-06-2022",
+		shiftType: "Night",
+		startTime: "05.28 pm",
+		endTime: "08.28 am",
+	},
+	{
+		id: "4",
+		date: "12-06-2022",
+		shiftType: "Night",
+		startTime: "05.28 pm",
+		endTime: "08.28 am",
+	},
+]
+
+export const shiftLogTitles = [
+		"Date",
+		"Shift Type",
+		"Shift Start Time",
+		"Shift End Time",
+]
+
+// parking log
+export const parkingLogTitles = [
+	"Employee ID",
+	"Name",
+	"Vehicle ID",
+	"IN/OUT",
+	"Date",
+	"Time"
+]
+
+export const parkingLogData = [
+	{
+		id: "1",
+		employeeId: "E0001",
+		name: "M.R. Perera",
+		vehicleId: "CDB 4306",
+		inOut: "IN",
+		date: "23-06-2022",
+		time: "12.30 pm",
+	},
+	{
+		id: "2",
+		employeeId: "E0405",
+		name: "J.D. Wijesekara",
+		vehicleId: "AAB 5546",
+		inOut: "IN",
+		date: "13-02-2022",
+		time: "08.30 am",
+	},
+	{
+		id: "3",
+		employeeId: "E0056",
+		name: "M.Rohan Perera",
+		vehicleId: "CRE 5448",
+		inOut: "OUT",
+		date: "04-06-2022",
+		time: "09.30 am",
+	},
+	{
+		id: "2",
+		employeeId: "E0405",
+		name: "J.D. Wijesekara",
+		vehicleId: "AAB 5546",
+		inOut: "IN",
+		date: "13-02-2022",
+		time: "08.30 am",
+	},
+	{
+		id: "3",
+		employeeId: "E0056",
+		name: "M.Rohan Perera",
+		vehicleId: "CRE 5448",
+		inOut: "OUT",
+		date: "04-06-2022",
+		time: "09.30 am",
+	},
+];
+
+export const employeeTitles = [
+	"Employee ID",
+	"Name",
+	"Vehicle ID",
+	"Phone No."
+];
+
+export const employeeData = [
+	{
+		id: "1",
+		employeeId: "E0001",
+		name: "M.R. Perera",
+		vehicleId: "CDB 4306",
+		phoneNo: "0778435221",
+	},
+	{
+		id: "2",
+		employeeId: "E4501",
+		name: "M.R. Wijesekara",
+		vehicleId: "DF 4556",
+		phoneNo: "0722375621",
+	},
+	{
+		id: "3",
+		employeeId: "E0067",
+		name: "M.Rohan Perera",
+		vehicleId: "CJJ 7308",
+		phoneNo: "0778435523",
+	},
+];
